becdf2961a723710be6204919dd5ec8610dd8a7f
Gabriel Ng <gabrielng15@gmail.com>
2018-09-19 12:35:57 -0700

    #23 added icons to footer

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
     console.log("hello");
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
